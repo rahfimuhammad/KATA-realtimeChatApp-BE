deaae53765a9f5eed663cbdf2eb6e051ff156889
rahfimuhammad <rahfi.muhammad@alumni.ui.ac.id>
2024-03-06 01:33:57 +0700

    make it better

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: 'http://localhost:3000', // Ganti dengan alamat aplikasi React Anda
+    origin: ['http://localhost:3000', 'https://5814-111-94-116-173.ngrok-free.app/'],
     methods: ['GET', 'POST'],
   },
 });
