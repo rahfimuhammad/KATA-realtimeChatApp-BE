ae035657a7f34da3858610a71ceeb05241fe7a2a
rahfimuhammad <rahfi.muhammad@alumni.ui.ac.id>
2024-03-09 00:12:26 +0700

    make it better

--- index.js
+++ index.js
@@ -18,15 +18,18 @@ io.on('connection', (socket) => {
   const id = socket.handshake.query.id;
   socket.join(id);
 
-  socket.on('send-message', ({ recipients, text }) => {
+  socket.on('send-message', ({ messageId, recipients, text }) => {
     recipients.forEach((recipient) => {
       const newRecipients = recipients.filter((r) => r !== recipient);
       newRecipients.push(id);
       socket.broadcast.to(recipient).emit('receive-message', {
         recipients: newRecipients,
+        messageId,
         sender: id,
         text,
       });
+
+      socket.emit('message-sent', messageId);
     });
   });
 });
